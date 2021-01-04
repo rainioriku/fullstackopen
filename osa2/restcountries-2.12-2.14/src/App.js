11a9543adfb79d83ef181b48a9fb3020ecd52ea8
rainioriku <rkurainio@gmail.com>
2021-01-04 15:36:08 +0200

    update restcountries working fully with APIs now.

--- osa2/restcountries-2.12-2.14/src/App.js
+++ osa2/restcountries-2.12-2.14/src/App.js
@@ -27,10 +27,44 @@ const App = () => {
               handleSearchChange={handleSearchChange}/>
       <ListCountries countries={countries}
                      search={newSearch}/>
-    
     </div>
   )
+}
 
+const Weather = (props) => {
+  const [temperature, setTemperature] = useState(0)
+  const [wind, setWind] = useState(0)
+  const [windDirection, setWindDirection] = useState("")
+
+  const api_key = process.env.REACT_APP_API_KEY
+  const parsed_api_key = api_key.split(" ")[0]
+  console.log("apikey: ", parsed_api_key)
+  let country = props.country
+  let capital = country.capital
+  console.log("capital: ", capital)
+
+  //Update when weather re-render
+  //or when country changes
+  useEffect(() => {
+    axios
+      .get("http://api.weatherstack.com/current?access_key=" + parsed_api_key + "&query=" + capital)
+      .then(response => {
+        const apiResponse = response.data
+        console.log(apiResponse)
+        setTemperature(apiResponse.current.temperature)
+        setWind(apiResponse.current.wind_speed)
+        setWindDirection(apiResponse.current.wind_dir)
+      })
+  }, [country])
+
+  return (
+    <div>
+      <h2>Weather in {capital}</h2>
+      <p><b>temperature:</b>{temperature} Celsius</p>
+
+      <p><b>wind:</b>{wind} mph direction {windDirection}</p>
+    </div>
+  )
 }
 
 const Search = (props) => {
@@ -48,15 +82,37 @@ const Search = (props) => {
 //COMPONENT FOR LISTING COUNTRIES
 const ListCountries = (props) => {
   const [filterCountries, setFilterCountries] = useState([props.countries])
-  console.log("filterCountries len: ", filterCountries.length)
-
+  
   //run when props.search filter changes
   useEffect(() => {
-    let countries = props.countries
+    const countries = props.countries
     let filteredCountries = countries.filter(country =>
       country.name.toLowerCase().includes(props.search.toLowerCase()))
     setFilterCountries(filteredCountries)
-  }, [props.search])
+  }, [props.search, props.countries])
+
+  //BUTTON HANDLER-->
+  const showCountry = (e) => {
+    //Parse country out of even info (probably not the best way)
+    let data = e.target.parentElement.innerText
+    let postfix = " show"
+    let postfixIndex = data.search(postfix)
+    let parsedCountry = data.substring(0, postfixIndex)
+    console.log(parsedCountry)
+    
+    //find this country object from 250 countries arr
+    let foundCountry = props.countries.find((country, index) => {
+      if(country.name === parsedCountry){
+        return true
+      }
+    })
+    console.log([foundCountry])
+    let wrap = []
+    wrap[0] = foundCountry
+    setFilterCountries(wrap)
+    console.log("filterCountries: ", filterCountries.length)
+
+  }
 
   //RENDER CONDITIONS-->
   if(filterCountries.length === props.countries.length){
@@ -69,16 +125,36 @@ const ListCountries = (props) => {
   if(filterCountries.length === 0){
     return (
       <div>
-        <p>No results</p>
+        <p></p>
       </div>
     )
   }
   if(filterCountries.length === 1){
-    return (
-      <div>
-        <p>1</p>
-      </div>
-    )
+    const country = filterCountries[0]
+    console.log("country: ", country, "length: ", country.length, "typeof: ", typeof(country))
+    if(country.name){
+      return (
+        <div>
+          <h1>{country.name}</h1>
+          <p> capital {country.capital}
+            <br/>population {country.population}
+          </p>
+          
+          <h2>languages</h2>
+          <ul>
+            {country.languages.map(language => 
+              <li>{language.name}</li>)}
+          </ul>
+          <img  className="flagimg"
+                alt="flag of the searched country"
+                src={country.flag} />
+          <Weather country={country}/>
+        </div>
+      )
+    }
+    else{
+      return (<div><p>one element</p></div>)
+    }
   }
   if(filterCountries.length > 10){
     return (
@@ -90,7 +166,11 @@ const ListCountries = (props) => {
   if(filterCountries.length < 10 && filterCountries.length > 1){
     return (
       <div>
-        <p>1-10</p>
+        <p>
+          {filterCountries.map(country =>
+            <p>{country.name} <button onClick={showCountry}>show</button></p>
+          )}
+        </p>
       </div>
     )
   }
